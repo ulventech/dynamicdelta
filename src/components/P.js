8a19de0d32d848d9d0587e25998b31fdf5c32c08
Mervlim <melzlim@hotmail.com>
2018-01-17 13:55:41 +0800

    edited regex for links

--- src/components/P.js
+++ src/components/P.js
@@ -14,7 +14,7 @@ export class P extends React.Component {
   formatText = (text) => {
     let newStr = text.replace(/(?:\r\n|\r|\n)/g, "<br />");
     const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-    newStr = newStr.replace(expression, "<a href='$1'>$1</a>");
+    newStr = newStr.replace(expression, "<a href='$1$2'>$1$2</a>");
     newStr = newStr.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
     newStr = newStr.replace(/~~(.*?)~~/g, "<i>$1</i>");
     return newStr;
--- standalone/react-tooltip.js
+++ standalone/react-tooltip.js
@@ -12412,7 +12412,7 @@ var P = exports.P = function (_React$Component) {
     _this.formatText = function (text) {
       var newStr = text.replace(/(?:\r\n|\r|\n)/g, "<br />");
       var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-      newStr = newStr.replace(expression, "<a href='$1'>$1</a>");
+      newStr = newStr.replace(expression, "<a href='$1$2'>$1$2</a>");
       newStr = newStr.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
       newStr = newStr.replace(/~~(.*?)~~/g, "<i>$1</i>");
       return newStr;
