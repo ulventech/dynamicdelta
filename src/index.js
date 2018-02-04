baac4fbb3e213b44323f9f13ac7d52cf56f1bd1c
Daniel Dyrnes <thedanielforum@gmail.com>
2018-02-04 21:25:34 +0800

    Exported Img

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@
 import DynamicDelta from './core/DynamicDelta';
 import P from './components/P';
 import H1 from './components/H1';
+import Img from './components/Img';
 
 module.exports = {
   DynamicDelta,
   P,
   H1,
+  Img,
 };
