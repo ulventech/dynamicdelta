30f5231a03a1b4184eac577eb3b48d8f61c804a1
Mervlim <melzlim@hotmail.com>
2018-03-02 17:39:52 +0800

    added H2 to H6 components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 'use strict';
 
 import DynamicDelta from './core/DynamicDelta';
-import P from './components/P';
 import H1 from './components/H1';
+import H2 from './components/H2';
+import H3 from './components/H3';
+import H4 from './components/H4';
+import H5 from './components/H5';
+import H6 from './components/H6';
+import P from './components/P';
 import Img from './components/Img';
 
 module.exports = {
   DynamicDelta,
-  P,
   H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  P,
   Img,
 };
